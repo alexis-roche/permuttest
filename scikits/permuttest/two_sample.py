fixed numerical instability in Hotelling statistic due to Cholesky decomposition

scikits/permuttest/two_sample.py
@@ -1,23 +1,6 @@
 import numpy as np
-from scipy.linalg import cho_solve, cho_factor
 
 TINY = 1e-100
-
-
-def _hotelling_t_square(Y, n1):
-    M1 = np.mean(Y[0:n1, ...], 0)
-    M2 = np.mean(Y[n1::, ...], 0)
-    dM = M1 - M2
-    n = Y.shape[0]
-    T2 = np.zeros(Y.shape[1])
-    for i in range(Y.shape[1]):
-        y = Y[:, i, :]
-        y -= np.mean(y, 0)
-        V = np.dot(y.T, y) / (n - 2)
-        L, lower = cho_factor(V)  # V = L L.T
-        x = cho_solve((L, lower), dM[i, :])
-        T2[i] = n * np.sum(x ** 2)
-    return T2
 
 
 def design_matrix(n, n1, confounds):
@@ -65,9 +48,9 @@ def hotelling_t_square(Y, n1, confounds=None):
         beta = np.dot(pinvX, y)
         res = y - np.dot(X, beta)
         V = np.dot(res.T, res) / (n - X.shape[1])
-        L, lower = cho_factor(V)  # V = L L.T
-        x = cho_solve((L, lower), beta[0, :])
-        T2[i] = factor * np.sum(x ** 2)
+        x = np.linalg.solve(V, beta[0, :])
+        T2[i] = factor * np.dot(beta[0, :], x)
+
     return T2
 
 

tests/unittests.py
@@ -6,6 +6,9 @@ from scikits.permuttest.two_sample import (permutation_test,
                                            _hotelling_t_square)
 
 
+
+
+
 """
 Y = np.random.normal(size=(10, 10000))
 t = t_stat(Y, 5)
@@ -13,29 +16,17 @@ t = t_stat(Y, 5)
 
 from scipy.linalg import cho_solve, cho_factor
 
+
+def test_hotelling_t_square():
+    Y = np.random.normal(size=(10, 100, 1))
+    t2 = t_stat(Y.squeeze(), 5) ** 2
+    T2 = hotelling_t_square(Y, 5)
+    return t2, T2
+
+"""
 # 10 subjects, 7 regions, 2 contrasts
 Y = np.random.normal(size=(10, 100, 2))
 
-n = Y.shape[0]
-n1 = 5
-age = np.random.rand(10)
-X = design_matrix(n, n1, confounds=(age,))
-invXtX = np.linalg.inv(np.dot(X.T, X))
-pinvX = np.dot(invXtX, X.T)  # pseudo-inverse
-T2 = np.zeros(Y.shape[1])
-factor = 1 / invXtX[0, 0]
-for i in range(Y.shape[1]):
-    y = Y[:, i, :]
-    beta = np.dot(pinvX, y)
-    res = y - np.dot(X, beta)
-    V = np.dot(res.T, res) / (n - X.shape[1])
-    L, lower = cho_factor(V)  # V = L L.T
-    x = cho_solve((L, lower), beta[0, :])
-    T2[i] = factor * np.sum(x ** 2)
-
-
-
-"""
 # test hotelling stat without confounds
 T0 = hotelling_t_square(Y, 5)
 